459f47616f6b81c53fd5ad044a53137cd0c1c224
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-18 14:27:24 -0400

    moorefield: idle: Remove Asus change to cpuidle
    Change-Id: I0da1f5ba9d54cc85150cb5261ae4cb79c4659c33

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -239,7 +239,6 @@ static int poll_idle(struct cpuidle_device *dev,
 
 	t1 = ktime_get();
 	local_irq_enable();
-
 	while (!need_resched())
 		cpu_relax();
 
@@ -531,8 +530,6 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 static void smp_callback(void *v)
 {
 	/* we already woke the CPU up, nothing more to do */
-	if (is_idle_task(current))
-		set_tsk_need_resched(current);
 }
 
 /*
